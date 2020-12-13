7cc96d3a0310670e92d5ca59058de97b9dab1a28
Ulli-H <uanklam@yahoo.de>
2020-12-13 16:36:08 +0100

    adding create

--- server.js
+++ server.js
@@ -6,22 +6,34 @@ const app = express();
 
 const MongoClient = require('mongodb').MongoClient
 
-MongoClient.connect('mongodb-connection-string', (err, client) =>{
-    // do something
-})
+const mongodbConnectionString = 'mongodb+srv://yoda:MayTheForse99@cluster0.m5xwj.mongodb.net/test?retryWrites=true&w=majority'
 
-app.listen(3000, function() {
-    console.log('listening on 3000')
-})
+MongoClient.connect(mongodbConnectionString, {
+useUnifiedTopology: true})
+    .then(client =>{
+        console.log('Connected to Database')
+        const db = client.db('my-quotes')
+        const quotesCollection = db.collection('quotes')
+
+ 
+        app.use(bodyParser.urlencoded( { extended: true }))
+        
+        app.get('/', (req, res) => {
+            res.sendFile(__dirname + '/index.html' ) })
+        
+        app.post('/quotes', (req, res) => {
+            quotesCollection.insertOne(req.body)
+                .then(result => {
+                    res.redirect('/')
+                })
+                .catch(error => console.error(error))
+        })
+
+        app.listen(3000, function() {
+            console.log('listening on 3000') })
+    })
+    .catch(error => console.error(error))
+    
 
 
-app.use(bodyParser.urlencoded( { extended: true }))
 
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html' )
-})
-
-app.post('/quotes', (req, res) => {
-    console.log(req.body)
-})
