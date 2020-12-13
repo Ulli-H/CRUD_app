e43da891ad2f055ccf2e97c72e70b47b68a6e269
Ulli-H <uanklam@yahoo.de>
2020-12-13 13:32:08 +0100

    adding progress

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 console.log('may node be with you')
 
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
+
+const MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('mongodb-connection-string', (err, client) =>{
+    // do something
+})
 
 app.listen(3000, function() {
     console.log('listening on 3000')
 })
 
+
+app.use(bodyParser.urlencoded( { extended: true }))
+
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html' )
+})
+
+app.post('/quotes', (req, res) => {
+    console.log(req.body)
 })
